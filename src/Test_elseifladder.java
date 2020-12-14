f4437aee00d6d76e0711a91751035536cec3524a
hiren1984 <hirenm.rajput@gmail.com>
2020-12-14 11:49:24 -0500

    elseifladder

---
+++ src/Test_elseifladder.java
@@ -0,0 +1,19 @@
+public class Test_elseifladder {
+    public static void main(String[] args) {
+        int marks = 95;
+        if (marks < 60) {
+            System.out.println("Fail");
+        } else if (marks >= 60 && marks < 70) {
+            System.out.println("D grade ");
+        } else if (marks >= 70 && marks < 80) {
+            System.out.println("B grade");
+        } else if (marks >= 80 && marks < 90) {
+            System.out.println("A grade");
+        } else if (marks >= 90 && marks <= 100) {
+            System.out.println("A+ grade");
+        } else {
+            System.out.println("Invalid");
+        }
+
+    }
+        }
