b320ad0ce2fd9982c6165dcfb1541206a2e172a3
hiren1984 <hirenm.rajput@gmail.com>
2020-12-16 21:08:32 -0500

    Aggregation

---
+++ src/SwappingTwoNumbers.java
@@ -0,0 +1,15 @@
+public class SwappingTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("Before swapping values are.." + a + " " + b);
+
+        //Logic1 - Create Third Variable
+        int t = a;
+        a = b;
+        b = t;
+        System.out.println("After swapping values are.."+ a +" "+b);
+
+
+    }
+}
